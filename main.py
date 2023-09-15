5d667cc8a2e7f446bbf3c73f412a609f733d39cd
Aaron Ma <aaronma113@gmail.com>
2023-09-15 00:11:45 -0700

    Updated main.py
    Included total duration times for unique titles

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ import numpy as np
 
 df = pd.read_csv('ViewingActivity.csv')
 
-users = df['Profile Name'].unique()
-
 df = df[df['Supplemental Video Type'].isna()]
+
 df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-
-
 def duration_hrs(df):
-    
     df['Duration'] = df['Duration'].astype(str)
 
     hr = df['Duration'].str.split(':').str[0].astype(int)
@@ -23,12 +19,27 @@ def duration_hrs(df):
     return df
 
 df['Year'] = df['Start Time'].dt.strftime('%Y')
+
+users = df['Profile Name'].unique()
 years = df['Year'].unique()
 
+
+
+df = df.drop(columns= ['Attributes', 'Bookmark', 'Latest Bookmark'])
+
+duration_hrs(df)
+sum_durations_df = df[['Title', 'Duration (hrs)']]
+sum_durations_df = sum_durations_df.groupby(by = ['Title']).sum().sort_values(by= ['Duration (hrs)'], ascending= True).rename(columns={'Duration (hrs)': 'Total Duration (hrs)'})
+
+print(sum_durations_df.head(20))
+
+
+
+
+### Stats
+number_content_pieces_watched = len(df['Title'].unique())
+
 # print(duration_hrs(df).head(10))
-print(len(df['Title'].unique()))
-
-
 # for user in users:
 #     user_df = df[df['Profile Name'] == user]
 #     print(user_df)
